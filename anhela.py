5ccbc0ee5e50a4db8b881695caf85eb3de67c046
gabriel montagné láscaris-comneno <gabriel@tibas.london>
2019-10-06 02:16:30 +0200

    add node

--- anhela.py
+++ anhela.py
@@ -1,6 +1,8 @@
 import bpy
 import nodeitems_builtins
 from bpy.types import NodeTree, Node, NodeSocket
+import nodeitems_utils
+from nodeitems_utils import NodeCategory, NodeItem
 
 print(nodeitems_builtins, '<<<')
 
@@ -8,16 +10,45 @@ class AnhelaNodeTree(NodeTree):
     bl_label = "Anhela Node Tree"
     bl_icon = "NODETREE"
 
+class AnhelaNode:
+    @classmethod
+    def poll(cls, ntree):
+        return ntree.bl_idname == "AnhelaNodeTree"
+
+class AnhelaNoteNode(Node, AnhelaNode):
+    bl_label = "Note Node"
+    bl_icon = 'TEXT'
+
+
+class AnhelaNodeCategory(NodeCategory):
+    @classmethod
+    def poll(cls, context):
+        return context.space_data.tree_type == 'AnhelaNodeTree'
+
+
+node_categories = [
+    AnhelaNodeCategory(
+        'GENERIC', 
+        "Generic", 
+        items=[NodeItem('AnhelaNoteNode')]
+    )
+]
+
 classes = (
     AnhelaNodeTree,
+    AnhelaNoteNode
 )
 
 def register():
     from bpy.utils import register_class
     for cls in classes:
         register_class(cls)
+    
+    nodeitems_utils.register_node_categories('ANHELA_NODES', node_categories)
 
 def unregister():
+    nodeitems_utils.unregister_node_categories('ANHELA_NODES')
+
     from bpy.utils import unregister_class
     for cls in reversed(classes):
         unregister_class(cls)
