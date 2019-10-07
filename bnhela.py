03243862e2e51358d6f72a15881463813a6f508f
gabriel montagné láscaris-comneno <gabriel@tibas.london>
2019-10-07 17:44:42 +0200

    First automatic update of target scene

--- bnhela.py
+++ bnhela.py
@@ -1,3 +1,4 @@
+from bpy.props import StringProperty
 from bpy.types import NodeTree, Node, NodeSocket, NodeSocketString, NodeReroute
 from nodeitems_utils import NodeCategory, NodeItem
 import bpy
@@ -6,21 +7,52 @@ import nodeitems_utils
 
 BNT = "Bnhela Node Tree"
 
+def get_other_socket(socket):
+    """
+    Get next real upstream socket.
+    Will return None if there isn't a another socket connect
+    so no need to check socket.links
+
+    From, https://blender.stackexchange.com/a/153909/19669
+    """
+
+    if not socket.is_linked:
+        return None
+    if not socket.is_output:
+        other = socket.links[0].from_socket
+    else:
+        other = socket.links[0].to_socket
+
+    if other.node.bl_idname == 'NodeReroute':
+        if not socket.is_output:
+            return get_other_socket(other.node.inputs[0])
+        else:
+            return get_other_socket(other.node.outputs[0])
+    else:
+        return other
+
+
 class BnhelaNodeTree(NodeTree):
     bl_label = BNT
     bl_icon = "EVENT_B"
+
+    def update(self):
+        print('\n' * 2, '=========== Node tree update')
 
 class BnhelaCharacterSocket(NodeSocket):
 
     bl_label = 'Bnhela Character Socket'
 
+    def on_name_update(self, context):
+        print('Name updated', self.character_name)
+
+    character_name: StringProperty('Name', update=on_name_update, default='')
+
     def draw_color(self, context, node):
         return (0.6, 0.0, 0.0, 1.0)
 
     def draw(self, context, layout, node, text):
-        layout.label(text=text)
-
-
+        layout.label(text=self.character_name)
 
 
 class BnhelaNode:
@@ -35,9 +67,36 @@ class BnhelaCharacterNode(Node, BnhelaNode):
     bl_label = "Character Node"
     bl_icon = 'OUTLINER_DATA_ARMATURE'
 
+    def on_character_name_update(self, context):
+        print('on character_name update (on node)', self, self.character_name)
+        self.update()
+
+    character_name: StringProperty(update=on_character_name_update)
+
+    def draw_buttons(self, context, layout):
+        layout = layout
+        layout.prop(self, 'character_name', text='Namε')
+
     def init(self, context):
         self.outputs.new('BnhelaCharacterSocket', 'Name')
 
+    def update(self):
+        print('Char node update', self.name)
+        print('   ... out socket', self.outputs['Name'])
+
+
+        name_socket = self.outputs['Name']
+        other_socket = get_other_socket(name_socket)
+
+        if not other_socket:
+            print('No other socket, ciao!')
+            return
+
+        if name_socket.bl_idname != other_socket.bl_idname:
+            print('Socket mismatch, ouch X-(')
+            return
+
+        other_socket.character_name = self.character_name
 
 
 class BnhelaSceneNode(Node, BnhelaNode):
@@ -46,10 +105,14 @@ class BnhelaSceneNode(Node, BnhelaNode):
     bl_icon = 'VIEW_CAMERA'
 
     def init(self, context):
+
+        self.outputs.new('BnhelaCharacterSocket', 'Main Character')
+        self.outputs.new('BnhelaCharacterSocket', '2nd Character')
+        self.outputs.new('BnhelaCharacterSocket', '3rd Character')
+
         self.inputs.new('BnhelaCharacterSocket', 'Main Character')
         self.inputs.new('BnhelaCharacterSocket', '2nd Character')
         self.inputs.new('BnhelaCharacterSocket', '3rd Character')
-
 
 
 
